5a05db316213bf023cc77fe671bc98c1abd4c536
Gabo <@>
2023-10-30 21:40:29 -0600

    dalkjakl

--- P2/src/index.js
+++ P2/src/index.js
@@ -1,9 +1,86 @@
-$("documento").ready(function () {
-    fetch('./src/json/usuarios.json')
-    .then((res) => {
-        return res.json();
-    })
-    .then((data) => {
-        console.log(data);
-    })  
+//Datos Iniciales
+let usuarios;
+let productos;
+let activo = "opcionProductos";
+//Evento inicialización
+$("document").ready(function () {
+     //Datos del LocalStorage / SessionStorage
+    if (localStorage.getItem('productos'))
+        productos = JSON.parse(localStorage.getItem('productos'));
+    else
+        productos = [];
+    if (sessionStorage.getItem('sesion'))
+        window.location.replace('index.html');
+    //RenderizarProductos();
 });
+//Funciones
+///Escribe en local storage, método genérico
+const Escribir = (item, valor) =>
+{
+    localStorage.setItem(item,valor);
+}
+const RenderizarProductos = () => {
+    let htmlInterno = "";
+    if (localStorage.getItem('productos'))
+    {
+        productos.forEach(p => function(){
+            htmlInterno += `<div class="card col-6" style="width: 18rem;">
+                        <img class="card-img-top" src=" ${p.urlImagen}" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">${p.descripcion}</h5>
+                        <p class="card-text">${p.descripcionLarga}</p>
+                        <button href="#" class="btn btn-danger" onclick=EliminarProducto('${p.id}')>Eliminar</button>
+                        </div>
+                    </div>`;
+             
+        });
+    }
+    else
+    {
+        $('#listadoProductos').addClass("text-center")
+        htmlInterno = `<h5>No hay productos registrados</h5>`;
+    }
+    $('#listadoProductos').html(htmlInterno);
+}
+const LimpiarFormProductos = () => {
+    $('#txtDescripcion').val('');
+    $('#txtDescripcionLarga').val('');
+    $('#txtDescripcionLarga').val('');
+    $('#txtUrlImagen').val('');
+}
+const IngresarProducto = () => {
+    //Declaración de objeto producto
+    let bandera = true;
+    let producto = {
+        id: productos.length,
+        descripcion: $('#txtDescripcion').val(),
+        descripcionLarga: $('#txtDescripcionLarga').val(),
+        precio: $('#txtDescripcionLarga').val(),
+        urlImagen: $('#txtUrlImagen').val()
+    };
+    let arregloProd = Object.values(producto);
+    arregloProd.forEach(val =>{
+        if (val === "" || val === null)
+            bandera = false;
+    })
+    if (bandera)
+    {
+        //Escritura en el arreglo
+        productos.push(producto);
+        //Escritura en local storage
+        let datos = JSON.stringify(productos);
+        Escribir('productos', datos);
+        LimpiarFormProductos();
+    }
+    else
+        alert("");
+}
+///Método de navegación
+const CambioVentana = (nuevaOpcion) =>{
+    if (nuevaOpcion != activo)
+    {
+        $('#'+nuevaOpcion).css('display','flex');
+        $('#'+activo).css('display','none');
+        activo = nuevaOpcion;
+    }
+}
--- P2/src/sesion.js
+++ P2/src/sesion.js
@@ -1,9 +1,57 @@
-$("documento").ready(function () {
-    fetch('./src/json/usuarios.json')
-    .then((res) => {
-        return res.json();
-    })
-    .then((data) => {
-        console.log(data);
-    })  
-});
+const EscribirSesion = (usuario) => {
+    sessionStorage.setItem('sesion', usuario);
+}
+const EscribirUsuarios = (usuarios) => {
+    localStorage.setItem('usuarios', usuarios);
+}
+const Usuarios = () => {
+    if (localStorage.getItem('usuarios'))
+        return JSON.parse(localStorage.getItem('usuarios'));
+}
+const Registro = () => {
+    let usuarios = Usuarios();
+    usuarios.forEach(u => {
+        if(u.email === $('txtEmail').val())
+            return false;
+    });
+    if ($('txtPass').val() != $('txtConfirmarPass').val())
+    {
+        return false;
+    }
+    let usuarioRegistrar = {
+        nombre: $('txtNombre').val(),
+        apellido: $('txtApellido').val(),
+        email: $('txtEmail').val(),
+        pass: $('txtPass').val(), 
+        admin: false
+    }
+    usuarios.push(usuarioRegistrar);
+    EscribirUsuarios(JSON.stringify(usuarioRegistrar));
+}
+const Ingreso = () => {
+    let usuario = {
+        email:  $('txtEmailLogin').val(),
+        pass: $('txtPassLogin').val()
+    }
+    let usuarios = Usuarios();
+    console.log(usuarios);
+    console.log(usuario);
+    usuarios.forEach(u => {
+        if (u.email === usuario.email)
+        {
+            if (u.pass === usuario.pass)
+            {
+                EscribirSesion(JSON.stringify(u));
+            }
+            else
+            {
+                alert("no coinciden")
+            }
+        }
+    });
+    alert("no hay datos")
+}
+const CerrarSesion = () => {
+    sessionStorage('sesion', 'null');
+    window.location.replace('index.html')
+}
